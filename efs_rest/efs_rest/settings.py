6cf25cfd74319b81605b348b680ed3e15bb150d1
Your Name <anits1088@gmail.com>
2020-08-02 13:52:56 -0500

    correcting Settings

--- efs_rest/efs_rest/settings.py
+++ efs_rest/efs_rest/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_REDIRECT_URL = '/'
 
-EST_FRAMEWORK = {
+REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
