5ca23da45638ff69003e64f9874be085bd86207e
Venkata Naga Sampath Ravi <vravi@unomaha.edu>
2020-08-07 10:25:33 -0500

    chnages in procfile and runtime

--- efs_rest/efs_rest/settings.py
+++ efs_rest/efs_rest/settings.py
@@ -137,6 +137,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL ='/media/'
 
+
 LOGIN_REDIRECT_URL = '/'
 django_heroku.settings(locals())
 
